ae581f99f2797a939610547806e8f8c77ea7049f
Dohun-BackEnd <kkaauu871@gmail.com>
2025-09-19 16:44:30 +0900

    Car 클래스 Move함수 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,5 +3,16 @@ package racingcar;
 import camp.nextstep.edu.missionutils.Randoms;
 
 class Car {
+    private static final int MOVING_FORWARD = 4;
+    private static final int start = 0;
+    private static final int end = 9;
 
+    private int Move_count;
+
+    void Move() {
+        int random = Randoms.pickNumberInRange(start, end);
+        if (random >= MOVING_FORWARD) {
+            Move_count += 1;
+        }
+    }
 }
