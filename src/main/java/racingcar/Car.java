4ca3b0f386b80e3c15b9e6d34d99b06303d4b32d
Dohun-BackEnd <kkaauu871@gmail.com>
2025-09-21 00:08:26 +0900

    Car 클래스 이름 입력 함수 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 class Car {
@@ -9,10 +10,26 @@ class Car {
 
     private int Move_count;
 
-    void Move() {
+    public void Move() {
         int random = Randoms.pickNumberInRange(start, end);
         if (random >= MOVING_FORWARD) {
             Move_count += 1;
         }
     }
+
+    public String[] get_Car_Names() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String Car_String = Console.readLine();
+        String[] Car_Names = Car_String.split(",");
+        String Car_Name;
+
+        for (int i = 0; i < Car_Names.length; i++) {
+            Car_Name = Car_Names[i];
+            if (Car_Name.length() > 5) {
+                throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+            }
+        }
+        return Car_Names;
+    }
+
 }
