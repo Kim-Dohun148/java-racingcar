3744597a61230717c0216585efd3e29cdb013313
Dohun-BackEnd <kkaauu871@gmail.com>
2025-09-21 16:36:43 +0900

    Racing 클래스 게임 종료 함수 구현

--- src/main/java/racingcar/Racing.java
+++ src/main/java/racingcar/Racing.java
@@ -2,7 +2,8 @@ package racingcar;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class Racing {
+
+public class Racing extends Car {
     public int get_round() {
         // 시도 횟수 입력
         System.out.println("시도할 횟수는 몇회인가요?");
@@ -16,20 +17,32 @@ public class Racing {
         return round;
     }
 
-    public void display(String[] Players) {
-        for (int i = 0; i < Players.length; i++) {
-            System.out.printf("%s : %s", Players[i], "-".repeat(2));
+    public void display(Car Players) {
+        for (int i = 0; i < Players.Car_Names.length; i++) {
+            System.out.printf("%s : %s", Players.Car_Names[i], "-".repeat(Players.Move_count[i]));
         }
     }
 
-    /*public void Start() {
-        int round = get_round();
+    public void End(Car Players) {
+        String[] Winner = new String[Players.Car_Names.length];
+        int max = Players.Move_count[0];
 
-        for (int i = 0; i < round; i++) {
-            //display();
-            //End();
+        for (int i = 0; i < Players.Car_Names.length; i++) {
+            for (int j = i + 1; j < Players.Car_Names.length; j++) {
+                if (Players.Move_count[i] >= Players.Move_count[j]) {
+                    max = Players.Move_count[i];
+                }
+            }
         }
-    }*/
+        for (int i = 0; i < Players.Car_Names.length; i++) {
+            if (Players.Move_count[i] == max) {
+                Winner[i] = Players.Car_Names[i];
+                }
+            }
+        }
+
+
+    }
 
 
 
